92bed1bb477df6e73cc90da787350d619f5868ce
NAVNEET GUPTA <gupta-navneet@live.com>
2018-10-04 20:56:24 -0700

    Allow adding middleware as part of the module

--- packages/redux-dynamic-modules/src/Contracts.ts
+++ packages/redux-dynamic-modules/src/Contracts.ts
@@ -1,65 +1,71 @@
-import { AnyAction, ReducersMapObject, Store } from "redux";
+import { AnyAction, ReducersMapObject, Store, Middleware } from "redux";
 
 /**
  * Represents a module which is set of reducers, sagas, inital actions and final actions
  */
 export interface IModule<State> {
-  /**
-   * Id of the module
-   */
-  id: string;
+    /**
+     * Id of the module
+     */
+    id: string;
 
-  /**
-   * Reducers for the module
-   */
-  reducerMap?: ReducersMapObject<State, AnyAction>;
+    /**
+     * Reducers for the module
+     */
+    reducerMap?: ReducersMapObject<State, AnyAction>;
 
-  /**
-   * These sagas are executed immediatly after adding the module to the store (before dispatching initial actions)
-   */
-  sagas?: ISagaRegistration<any>[];
+    /**
+     * Middlewares to add to the store
+     */
+    middlewares?: Middleware[];
 
-  /**
-   * These actions are dispatched immediatly after adding the module in the store
-   */
-  initialActions?: AnyAction[];
+    /**
+     * These sagas are executed immediately after adding the module to the store (before dispatching initial actions)
+     */
+    sagas?: ISagaRegistration<any>[];
 
-  /**
-   * These actions are dispatched immediatly before removing the module from the store
-   */
-  finalActions?: AnyAction[];
+    /**
+     * These actions are dispatched immediately after adding the module in the store
+     */
+    initialActions?: AnyAction[];
+
+    /**
+     * These actions are dispatched immediatly before removing the module from the store
+     */
+    finalActions?: AnyAction[];
 }
 
 export interface ISagaWithArguments<T> {
-  saga: (argument?: T) => Iterator<any>;
-  argument?: T;
+    saga: (argument?: T) => Iterator<any>;
+    argument?: T;
 }
 
 export type ISagaRegistration<T> = (() => Iterator<any>) | ISagaWithArguments<T>;
 
 export interface IDynamicallyAddedModule {
-  /**
-   * Call to remove the module from the store
-   */
-  remove: () => void;
+    /**
+     * Call to remove the module from the store
+     */
+    remove: () => void;
 }
 
 export interface IModuleManager {
-  /**
-   * Add the given module to the store
-   */
-  addModule: (...modules: IModule<any>[]) => IDynamicallyAddedModule
+    /**
+     * Add the given module to the store
+     */
+    addModule: (...modules: IModule<any>[]) => IDynamicallyAddedModule
 }
 
 export type IModuleStore<State> = Store<State> & IModuleManager & {
-  /**
-   * Remove all the modules from the store
-   */
-  dispose: () => void;
+    /**
+     * Remove all the modules from the store
+     */
+    dispose: () => void;
 };
 
 export interface IItemManager<T> {
-  getItems: () => T[];
-  add: (items: T[]) => void;
-  remove: (item: T[]) => void;
+    getItems: () => T[];
+    add: (items: T[]) => void;
+    remove: (item: T[]) => void;
+    dispose: () => void;
 }
