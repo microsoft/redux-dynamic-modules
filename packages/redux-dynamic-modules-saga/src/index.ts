1d7abdb74867b6261d0a08d4cba25a997e8b5562
Alex Bettadapur <alebet@microsoft.com>
2018-10-04 21:37:32 -0700

    Fix some more build issues

--- packages/redux-dynamic-modules-saga/src/index.ts
+++ packages/redux-dynamic-modules-saga/src/index.ts
@@ -1,5 +1,5 @@
 import { default as createSagaMiddleware, SagaMiddleware } from "redux-saga";
-import { IPlugin, IItemManager, getRefCountedManager } from "redux-dynamic-modules";
+import { IPlugin, IItemManager, getRefCountedManager, IModuleManager } from "redux-dynamic-modules";
 import { ISagaRegistration, ISagaModule } from "./Contracts";
 import { getSagaManager } from "./SagaManager";
 import { sagaEquals } from "./SagaComparer";
@@ -18,11 +18,17 @@ export function getSagaPlugin<C>(sagaContext?: C): IPlugin {
 
     return {
         middleware: [sagaMiddleware],
+
+        onModuleManagerCreated: (moduleManager: IModuleManager) => {
+            sagaContext["moduleManager"] = moduleManager;
+        },
+
         onModuleAdded: (module: ISagaModule<any>): void => {
             if (module.sagas) {
                 _sagaManager.add(module.sagas);
             }
         },
+
         onModuleRemoved: (module: ISagaModule<any>): void => {
             if (module.sagas) {
                 _sagaManager.remove(module.sagas);
