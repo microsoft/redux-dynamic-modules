564b60628052b49dfb0a75a767b6506ce9b266a6
Alex Bettadapur <alebet@microsoft.com>
2018-10-05 17:46:32 -0700

    Add rxjs, fix root epic

--- packages/redux-dynamic-modules-observable/src/EpicManager.ts
+++ packages/redux-dynamic-modules-observable/src/EpicManager.ts
@@ -1,5 +1,6 @@
 import { IItemManager, getStringRefCounter } from "redux-dynamic-modules";
-import { EpicMiddleware, Epic, combineEpics } from "redux-observable";
+import { EpicMiddleware, Epic } from "redux-observable";
+import { merge } from "rxjs";
 
 export interface IEpicManager extends IItemManager<Epic> {
     rootEpic: Epic;
@@ -12,7 +13,7 @@ export function getEpicManager(epicMiddleware: EpicMiddleware<any>): IEpicManage
     let runningEpics: Epic[] = [];
     const epicRefCounter = getStringRefCounter();
 
-    const rootEpic: Epic = combineEpics(...runningEpics);
+    const rootEpic: Epic = createRootEpic(runningEpics);
 
     return {
         getItems: (): Epic[] => runningEpics,
@@ -49,4 +50,28 @@ export function getEpicManager(epicMiddleware: EpicMiddleware<any>): IEpicManage
             runningEpics = null;
         }
     };
+}
+
+function createRootEpic(runningEpics: Epic[]): Epic {
+    const merger = (...args) => merge(
+        runningEpics.map(epic => {
+            //@ts-ignore
+            const output$ = epic(...args);
+            if (!output$) {
+                throw new TypeError(`combineEpics: one of the provided Epics "${epic.name || '<anonymous>'}" does not return a stream. Double check you\'re not missing a return statement!`);
+            }
+            return output$;
+        })
+    );
+
+    // Technically the `name` property on Function's are supposed to be read-only.
+    // While some JS runtimes allow it anyway (so this is useful in debugging)
+    // some actually throw an exception when you attempt to do so.
+    try {
+        Object.defineProperty(merger, 'name', {
+            value: '____MODULES_ROOT_EPIC',
+        });
+    } catch (e) { }
+
+    return merger;
 }
