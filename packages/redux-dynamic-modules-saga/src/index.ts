32e639169a5355e58e49594ae6d4b39d4a0a6763
Alex Bettadapur <alebet@microsoft.com>
2018-10-10 09:31:41 -0700

    Fix __DEV__ global

--- packages/redux-dynamic-modules-saga/src/index.ts
+++ packages/redux-dynamic-modules-saga/src/index.ts
@@ -1,2 +1,15 @@
+let target;
+if (window) {
+    target = window;
+}
+else {
+    //@ts-ignore
+    target = global;
+}
+
+if (!target["__DEV__"]) {
+    target["__DEV__"] = false;
+}
+
 export * from "./Contracts";
 export * from "./SagaExtension";
