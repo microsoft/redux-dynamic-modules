180950cc040abc65a22188e1aa413ec9d1773b51
Navneet Gupta <gupta-navneet@live.com>
2018-10-12 16:10:56 -0700

    Remove observable counter from App.js

--- packages/widgets-example/src/App.js
+++ packages/widgets-example/src/App.js
@@ -3,17 +3,13 @@ import { configureStore, DynamicModuleLoader } from "redux-dynamic-modules";
 import { getObservableExtension } from "redux-dynamic-modules-observable";
 import { getSagaExtension } from "redux-dynamic-modules-saga";
 import { Provider } from "react-redux";
-import logo from './logo.svg';
 import './App.css';
-import { ConnectedCounter } from "./widgets/observable-counter/component/observable-counter-component";
-import { getObservableCounterModule } from "./widgets/observable-counter/redux/observable-counter-module";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       hackerNews: false,
-      counter: false,
       weather: false
     };
 
@@ -26,8 +22,6 @@ class App extends Component {
         <div>
           <input type="checkbox" onChange={this.onHackerNewsToggled} />
           <label>Hacker News</label>
-          <input type="checkbox" onChange={this.onCounterToggled} />
-          <label>Counter</label>
           <input type="checkbox" onChange={this.onWeatherToggled} />
           <label>Weather</label>
         </div>
@@ -50,15 +44,6 @@ class App extends Component {
             )
           }
           {
-            this.state.counter && (
-              <div>
-                <DynamicModuleLoader modules={[getObservableCounterModule()]}>
-                  <ConnectedCounter />
-                </DynamicModuleLoader>
-              </div>
-            )
-          }
-          {
             this.state.weather && (
               <div>
                 Weather
@@ -73,11 +58,6 @@ class App extends Component {
   onHackerNewsToggled = () => {
     this.setState({ hackerNews: !this.state.hackerNews });
   };
-
-  onCounterToggled = () => {
-    this.setState({ counter: !this.state.counter });
-  }
-
   onWeatherToggled = () => {
     this.setState({ weather: !this.state.weather });
   }
