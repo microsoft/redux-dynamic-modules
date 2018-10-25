2d5c6a0c1cc7a68a6d9785c1c2c23e19631a3d74
Navneet Gupta <gupta-navneet@live.com>
2018-10-25 12:08:06 -0700

    First batch of demo steps

---
+++ packages/widgets-example/demosteps/hacker-news-index-imports.js
@@ -0,0 +1,6 @@
+import { ConnectedHackerNews } from "./component/hacker-news-component";
+import { getHackerNewsModule } from "./redux/hacker-news-module";
+import { DynamicModuleLoader } from "redux-dynamic-modules";
+import * as React from "react";
+
+export default ConnectedHackerNews;
---
+++ packages/widgets-example/demosteps/weather-index-imports.js
@@ -0,0 +1,6 @@
+import { ConnectedWeather } from "./component/weather-component";
+import { getWeatherModule } from "./redux/weather-module";
+import { DynamicModuleLoader } from "redux-dynamic-modules";
+import * as React from "react";
+
+export default ConnectedWeather;
