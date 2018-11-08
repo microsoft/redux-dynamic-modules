73c724bfcc86f481b5703dd16bad453ca4f58b25
NAVNEET GUPTA <gupta-navneet@live.com>
2018-11-07 20:29:06 -0800

    Update widgets example to use moduleEnhancer

--- packages/widgets-example/src/App.js
+++ packages/widgets-example/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import { createStore } from "redux";
 import { Provider } from "react-redux";
 // We will load the widgets async using react-loadable.
 import Loadable from "react-loadable";
 // createStore allows us to load/unload modules dynamically.
-import { createStore } from "redux-dynamic-modules";
+import { moduleEnhancer } from "redux-dynamic-modules";
 // Saga extension allows us to use Saga middleware in the module store.
 import { getSagaExtension } from "redux-dynamic-modules-saga";
 // Thunk extension allows us to use Thunk middleware in the module store.
@@ -25,7 +26,7 @@ class App extends Component {
      * The extensions are optional and you can choose extension based on the middleware you use
      * You can also build your own extensions for any other middleware e.g. redux-observable
      */
-    this.store = createStore({}, [getThunkExtension(), getSagaExtension()]);
+    this.store = createStore(null, {}, moduleEnhancer([getThunkExtension(), getSagaExtension()]));
   }
 
   render() {
