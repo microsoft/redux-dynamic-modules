4d87159043181b9c694537dff48a88c51ff445e9
Alex Bettadapur <alexbettadapur@gmail.com>
2018-11-11 15:22:58 -0800

    Actually add the file

---
+++ packages/redux-dynamic-modules/src/DynamicModuleLoader.tsx
@@ -0,0 +1,110 @@
+import * as React from "react";
+import * as PropTypes from "prop-types";
+import {
+  IModule,
+  IModuleStore,
+  IDynamicallyAddedModule
+} from "./Contracts";
+
+// @ts-ignore
+// Import ReactReduxContext from react-redux. If react-redux is less than version 6, ReactReduxContext will be null.
+import { ReactReduxContext } from "react-redux";
+
+export interface IDynamicModuleLoaderProps<OriginalState, AdditionalState> {
+  /** Modules that need to be dynamically registerd */
+  modules: IModule<AdditionalState>[];
+}
+
+// Use for version 5 and below
+export interface IDynamicModuleLoaderContext {
+  store: IModuleStore<any>;
+}
+
+/**
+ * The DynamicModuleLoader adds a way to register a module on mount
+ * When this component is initialized, the reducer and saga from the module passed as props will be registered with the system
+ * On unmount, they will be unregistered
+ */
+export class DynamicModuleLoader<
+  OriginalState,
+  AdditionalState
+  > extends React.Component<
+  IDynamicModuleLoaderProps<OriginalState, AdditionalState>
+  >
+{
+  // @ts-ignore 
+  // Version 5 and below
+  private static contextTypes = {
+    store: PropTypes.object
+  };
+
+  constructor(
+    props: IDynamicModuleLoaderProps<OriginalState, AdditionalState>,
+    context: IDynamicModuleLoaderContext
+  ) {
+    super(props, context);
+    if (!ReactReduxContext) {
+      if (!context.store) {
+        throw new Error("Could not load store from React context.");
+      }
+    }
+  }
+
+  /**
+   * Render a Redux provider
+   */
+  public render(): React.ReactNode {
+    if (ReactReduxContext) {
+      return (
+        <ReactReduxContext.Consumer>
+          {(context) => (
+            <DynamicModuleLoaderImpl
+              store={context.store}
+              modules={this.props.modules}
+            />
+          )
+          }
+        </ReactReduxContext.Consumer>
+      );
+    } else {
+      return (
+        <DynamicModuleLoaderImpl
+          store={this.context.store}
+          modules={this.props.modules}
+        />
+      )
+    }
+  }
+}
+
+
+interface IDynamicModuleLoaderImplProps {
+  /** Modules that need to be dynamically registerd */
+  modules: IModule<any>[];
+
+  store: IModuleStore<any>;
+}
+
+class DynamicModuleLoaderImpl extends React.Component<IDynamicModuleLoaderImplProps> {
+  private _addedModules?: IDynamicallyAddedModule;
+
+  constructor(props: IDynamicModuleLoaderImplProps) {
+    super(props);
+    const { modules, store } = props;
+    this._addedModules = store.addModules(modules);
+  }
+
+  public render(): React.ReactNode {
+    return this.props.children;
+  }
+
+  /**
+   * Unregister sagas and reducers
+   */
+  public componentWillUnmount(): void {
+    if (this._addedModules) {
+      this._addedModules.remove();
+      this._addedModules = undefined;
+    }
+  }
+}
