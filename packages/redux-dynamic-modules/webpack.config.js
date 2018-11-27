8e3bfb08b9d78439b390645f3faabf9898df7869
Alex Bettadapur <alexbettadapur@gmail.com>
2018-11-27 15:39:34 -0800

    Make prop types external

--- packages/redux-dynamic-modules/webpack.config.js
+++ packages/redux-dynamic-modules/webpack.config.js
@@ -18,6 +18,7 @@ module.exports = (env, argv) => {
         },
 
         externals: {
+            "prop-types": "prop-types",
             "react": "react",
             "redux": "redux",
             "react-redux": "react-redux",
