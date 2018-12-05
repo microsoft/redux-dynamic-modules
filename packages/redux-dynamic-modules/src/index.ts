18ded3f20a38ae9a808a7c91897cce87e1022c10
Navneet Gupta <13487927+navneet-g@users.noreply.github.com>
2018-12-05 12:16:27 -0800

    Dummy change to keep lerna happy

--- packages/redux-dynamic-modules/src/index.ts
+++ packages/redux-dynamic-modules/src/index.ts
@@ -5,3 +5,4 @@ export * from "./Utils/ComparableMap";
 export * from "./Utils/RefCounter";
 export * from "./Managers/MiddlewareManager";
 export * from "./Managers/RefCountedManager";
+//Dummy change to keep lerna happy
