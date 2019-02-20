4685e3cb91e8675b2a6090d42523061cef7ce968
Alexander Bettadapur <alebet@fb.com>
2019-02-20 14:23:36 -0800

    revert filter link

--- packages/todo-example/src/containers/todo/FilterLink.js
+++ packages/todo-example/src/containers/todo/FilterLink.js
@@ -3,7 +3,7 @@ import { setVisibilityFilter } from "../../modules/todo/actions";
 import Link from "../../components/todo/Link";
 
 const mapStateToProps = (state, ownProps) => ({
-    active: ownProps.filter === state.root.visibilityFilter,
+    active: ownProps.filter === state.todo.visibilityFilter,
 });
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
