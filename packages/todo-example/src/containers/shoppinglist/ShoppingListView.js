d474461ed16782c7ae021c453b16e872159314cc
Alexander Bettadapur <alebet@fb.com>
2019-02-20 14:23:11 -0800

    revert shopping list

--- packages/todo-example/src/containers/shoppinglist/ShoppingListView.js
+++ packages/todo-example/src/containers/shoppinglist/ShoppingListView.js
@@ -5,17 +5,13 @@ import VisibleItemsList from "./VisibleItemsList";
 import Footer from "../../components/shoppinglist/Footer";
 import { getShoppingListModule } from "../../modules/shoppinglist/shoppingListModule";
 
-const ShoppingListView = () => {
-    return (
-        <DynamicModuleLoader modules={[getShoppingListModule()]}>
-            <div>
-                <div>Shopping list view</div>
-                <AddItem />
-                <VisibleItemsList />
-                <Footer />
-            </div>
-        </DynamicModuleLoader>
-    );
-};
+const ShoppingListView = () => (
+    <DynamicModuleLoader modules={[getShoppingListModule()]}>
+        <div>Shopping list view</div>
+        <AddItem />
+        <VisibleItemsList />
+        <Footer />
+    </DynamicModuleLoader>
+);
 
 export default ShoppingListView;
